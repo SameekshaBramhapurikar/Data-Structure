73cda21e429b6917ff28cc51c361a991d917b7c6
Sameeksha <bramhapurikarsameeksha@gmail.com>
2026-06-06 23:25:34 +0530

    Create RemoveKdigits.java

---
+++ Stack&Queues/RemoveKdigits.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+    Stack<Character>st=new Stack<>();
+    int n=num.length();
+    for(int i=0;i<n;i++)
+    {
+        while(!st.isEmpty() && k>0 && (st.peek()-'0')>(num.charAt(i)-'0'))
+    {
+        st.pop();
+        k=k-1;
+    } 
+    st.push(num.charAt(i));        
+    }
+    while(k>0)
+    {
+        st.pop();
+        k--;
+    }
+    if(st.empty())
+    {
+        return "0";
+    }
+    StringBuilder res=new StringBuilder();
+    while(!st.isEmpty())
+    {
+      res.append(st.pop());  
+    }
+    res.reverse();
+
+    while(res.length() > 0 && res.charAt(0)=='0')
+        res.deleteCharAt(0);
+  
+
+    if(res.length()==0)
+        return "0";
+      
+    return res.toString();  
+    }
+}
