6bbbea30a5453d95e900186b0aef61af90cb754c
Sameeksha <bramhapurikarsameeksha@gmail.com>
2026-07-11 00:07:10 +0530

    Implement bipartite graph checking algorithm

---
+++ Graphs/BipartiteGraph.java
@@ -0,0 +1,45 @@
+class Solution {
+    private boolean check(int start,int V,int[][] graph,int color[])
+    {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(start);
+        color[start]=0;
+        while(!q.isEmpty())
+        {
+            int node=q.peek();
+            q.remove();
+
+            for(int it:graph[node])
+            {
+                if(color[it]== -1)
+                {
+                    color[it]= 1 - color[node];
+                    q.add(it);
+                }
+            
+                else if(color[it] == color[node])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int V=graph.length;
+        int color[]= new int[V];
+        for(int i=0;i<V;i++)
+        {
+            color[i]=-1;
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(check(i,V,graph,color)==false)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
